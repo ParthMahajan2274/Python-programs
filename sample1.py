446513f084dc81e1967b7855be347c914050f509
ParthMahajan2274 <152772670+ParthMahajan2274@users.noreply.github.com>
2023-12-03 14:08:59 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,11 @@
+a=1
+b=True
+c="Parth"
+d=None
+e=complex(8,2)
+print("a=",a,"b=",b,"c=",c,"d=",d,"e=",e)
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
+print("The type of d is ",type(d))
+print("The type of e is ",type(e))
